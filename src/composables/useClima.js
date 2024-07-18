224d38b20b1cff5a732c97f6956c4d748119757a
cesartomedes <cesartomedes@gmail.com>
2024-07-18 09:34:00 -0400

    app clima vue new

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -20,7 +20,7 @@ export default function useClima() {
 
         // obtener la lat, lon
         try {
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
             const {data} = await axios(url)
 
             const { lat , lon } = data[0]
